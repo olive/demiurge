3b62acdfaf3fffd8e3c66da5284f995a9358d108
olive <olivine.dogue@gmail.com>
2014-08-14 11:10:24 -0400

    indention

--- src/Demiurge/Common.hs
+++ src/Demiurge/Common.hs
@@ -9,6 +9,6 @@ data Rect = Rect Int Int Int Int
 rectToBorder :: Int -> Rect -> [XYZ]
 rectToBorder k (Rect x y width height) = do
     [(i, j, k) | i <- [x..x+width],
-              j <- [y..y+height],
-              i == 0 || j == 0 || i == width - 1 || j == height - 1]
+                 j <- [y..y+height],
+                 i == 0 || j == 0 || i == width - 1 || j == height - 1]
 
